de64aca2e56140cb4817118fd2272d6a4955f7da
Mesut Cevik <mesut.cevik@telekom.de>
2019-01-14 16:36:43 +0100

    Chapter Array finished.

--- Udemy-Java-Kurs-Array/src/com/company/AufbauEinesArrays.java
+++ Udemy-Java-Kurs-Array/src/com/company/AufbauEinesArrays.java
@@ -12,13 +12,22 @@ public class AufbauEinesArrays {
 
 
     // Array Deklaration
-    // So deklariert man einen Array: Datentyp[] Name = new Datentyp[Anzahl]
+    // Zeile 1: So deklariert man einen Array: Datentyp[] Name = new Datentyp[Anzahl]
+    // Zeile 2 ff.: So weist man einem Array-Feld einen Wert zu: Name[Index des Feldes] = Wert
     int[] zahlenArray = new int[7];
-    zahlenArray[3] = 0; // An der Position 3 den Wert 30 speichern. Der Wert des Inhalts vom Typ int.
+    zahlenArray[3] = 0; // An der Index-Position 3 der VAR zahlenArray speichere den Wert 30 (vom Typ int).
     zahlenArray[5] = 500;
     zahlenArray[0] = 40;
 
-    /** 1. MÖGLICHKEIT UM EINEN ARRAY ZU ERSTELLEN
+    // MERKE
+
+    /** ARRAY ERSTELLEN - VARIANTE 1
+     * Zuerst einen Array deklarieren (mit Datentyp und Variablennamen). Dabei wird auch die Datenstruktur festgelegt,
+     * d.h. die Menge der Felder wird angegeben, in denen Daten gespeichert werden sollen.
+     * Danach werden konkrete Werte den einzelnen Feldern der Array-Datenstruktur direkt zugewiesen. Nach dem Variablen-
+     * Namen des Arrays wird direkt in [] die Indexposition des Feldes angegeben, in die ein Wert zugewiesen wird.
+     * Der zugewiesene Wert steht rechts vom Zuweisungsoperator '=' ohne Klammern.
+     *
      * 1. Zeile deklariert nur den Array, welcher Daten (Werte) vom Typ int speichern kann und eine Aufnahmekapazität
      * für 7 Werte hat.
      * 2. Zeile und ff. weisen über den angegebenen Index dem Array jeweils einzelne Werte zu.
@@ -32,10 +41,15 @@ public class AufbauEinesArrays {
     zahlenArray1[6] = 0.98;
     System.out.println("Der fünfte Wert: " + zahlenArray1[4]);
 
-    /** 2. MÖGLICHKEIT UM EINEN ARRAY ZU ERSTELLEN
-     * Einem Array sofort nach der Deklaration Werte zuweisen.
-     * Die Eingabe der Werte erfolgt direkt nach dem Zuweisungsoperator '=' in {} und in der gewünschten
-     * (Index-)Reihenfolge.
+    /** ARRAY ERSTELLEN - VARIANTE 2
+     * Einen Array deklarieren und gleichzeitig Daten (Werte) zuweisen. Die Werte werden rechts vom Zuweisungsoperator
+     * '=' in geschweifte Klammern {} geschrieben. Dabei erfolgt die Zuweisung in der gewünschten (Index-)Reihenfolge.
+     * Bei dieser Variante wird die Datenstruktur des Arrays automatisch festgelegt, entsprechend der Anzahl und
+     * Reihenfolge der zugewiesenen Werte.
+     *
+     * 1. Zeile deklariert einen Array vom Datentyp String und gibt der Variable, in der die Werte gespeichert werden
+     * sollen, den Namen "personA". Rechts vom Zuweisungsoperator '=' werden in geschweifte Klammern sowie in der
+     * gewünschten (Index-)Reihenfolge die Strings angegeben. Die Datenstruktur dieses String-Arrays hat 3 Felder.
      */
 
     // Datenstruktur der VAR personA: Index 0 = "Peter", Index 1 = "Müller", Index 2 = "35".
@@ -45,5 +59,4 @@ public class AufbauEinesArrays {
     // Zugriff auf einen Wert in einem Array:
     // Name[Index des gewünschten Werts]
   }
-
 }
--- Udemy-Java-Kurs-Array/src/com/company/BeispielArray2.java
+++ Udemy-Java-Kurs-Array/src/com/company/BeispielArray2.java
@@ -1,4 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class BeispielArray2 {
+
+  public static void main(String[] args){
+
+    // Array Deklaration
+    // Datentyp[] Name = new Datentyp[Anzahl];
+
+    String[] fragen = {"Wieviel ist 5 + 4?", "Wieviel ist 3 * 5?"};
+    int[] antworten = {9, 15};
+
+    int count = 0;
+
+    while(count < fragen.length){
+    System.out.println(fragen[count]); // Die erste Frage wird ausgegeben.
+
+      Scanner scan = new Scanner(System.in);
+      int eingabeUser = scan.nextInt();
+
+      if(eingabeUser == antworten[count]){
+        System.out.println("Richtig!");
+        count++;
+      } else {
+        System.out.println("Falsch!");
+      }
+    }
+  }
 }
