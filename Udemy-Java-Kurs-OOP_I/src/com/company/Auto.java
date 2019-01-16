9a1b9dfdd66235008f95a8723e94d9a01cf1d701
Mesut Cevik <mesut.cevik@telekom.de>
2019-01-16 23:43:17 +0100

    Update.

--- Udemy-Java-Kurs-OOP_I/src/com/company/Auto.java
+++ Udemy-Java-Kurs-OOP_I/src/com/company/Auto.java
@@ -8,15 +8,90 @@ package com.company;
  * >> Ein Bauplan, anhand der Objekte mit bestimmten Eigenschaften generiert werden können.
  * >> Eine Klasse kann Variablen enthalten, die wir beim erstellen von Objekten nutzen und mit Werten versehen können.
  *
+ *
  * Wie wird ein Objekt erstellt? Siehe in die Datei Main.class!
  */
 
 public class Auto {
 
+  // Variablen der Klasse Auto: Das sind die Parameter dieser Klasse.
+  // Sie bestimmen die Eigenschaften eines zu erzeugenden Objekts dieser Klasse.
+  // Man nennt diese Variablen auch Eigenschaften oder Attribute der Klasse.
+
   int reifen;
   int preis;
-
   String marke;
 
+  // Wenn wir in einer Klasse keinen Konstruktur definieren, dann erstellt Java einen Standard Konstruktor
+  // im Hintergrund. Dann können trotz fehlen eines Konstruktors Opjekte von der Klasse erstellt werden, wenn die
+  // Klasse an anderer Stelle im Projekt oder Programm aufgerufen wird.
+
+  /** A. STANDARD-KONSTRUKTOR (KONSTRUKTOR OHNE PARAMETER)
+   * MERKE: Der Konstruktor initialisiert ein Objekt mit Werten. Mit Anfangswerten festsetzen.
+   * Java erstellt für jede Klasse automatisch einen Basis-Konstruktor, falls kein anderer Konstruktor definiert ist.
+   * Wenn aber ein Konstruktor (normalerweise mit Parametern) definiert wird, dann wird Java diesen berücksichtigen.
+   * Hier ist der Basis-Konstruktor explizit zu Anschauungszwecken definiert, obwohl weiter unten ein weiterer
+   * Konstruktor für die Klasse "Auto" definiert ist, welcher aber beim Aufrufen Parameterwerte benötigt.
+   */
+  public Auto(){
+
+  }
+
+  /** B. KONSTRUKTOR MIT PARAMETERN:
+   * Mit diesem Konstruktor von der Klasse "Auto" können wir Objekte erstellen. Mit dem Aufrufen des Konstruktors
+   * werden Parameterwerte übergeben werden. Dann wird beim Erstellen des Objekts dieser direkt mit Werten befüllt.
+   * Die Werte werden an VAR übergeben, die in der Auto-Klasse als VAR Eigenschaften bzw. definiert wurden.
+   *
+   * Das heißt, um ein Objekt von der Klasse Auto zu erstellen, muss dieser Konstruktor aufgerufen und ihm
+   * die festgelegten Parameter übergeben werden. Erst wenn alle erforderlichen Parameterwerte übergeben wurden,
+   * erstellt dieser Konstruktor mit diesen Werten ein Objekt generieren.
+   *
+   * Beispiel: Dem folgenden Konstruktor müssen beim Aufrufen die Parameterwerte für reifen, preis und marke übergen
+   * werden, damit ein Auto-Objekt initialisiert wird:
+   */
+  public Auto(int reifen, int preis, String marke){ // Diesem Konstruktor können wir Werte für die 3 VAR übergeben.
+    this.reifen = reifen;
+    this.preis = preis;
+    this.marke = marke;
+  }
+  /** this
+   * Das Schlüsselwort "this" veranlasst: Diese Objekt, das gerade mit dem new-Operator und dem Konstruktor erstellt
+   * wurde, soll in der VAR reifen den Wert vom Parameter "reifen" erhalten/zugewiesen bekommen. Der Parameterwert
+   * "reifen" wurde dem Konstruktor übergeben.
+   * Also die Objekt-Variable "reifen" (links von =) soll den Parameterwert "reifen" (rechts von =) erhalten.
+   *
+   * BEACHTE: Der Wert in "reifen" wird nicht der Variable "reifen" in der Klassen-Definition zugewiesen.
+   *
+   * Anders gesagt "this": Dieses Objekt, was gerade mit dem Konstruktor erstellt wurde, soll diese Variable (reifen)
+   * mit dem im Parameter "reifen" übergebenen Wert initialisiert bekommen.
+   * Jede VAR in der Konstruktor-Beschreibung wird für ein einzelnes Objekt initialisiert (mit einem
+   * ersten Wert befüllt), dass vom Konstruktor erstellt wird. Mit dieser Initialisierung wird nicht der oben
+   * defnierten Klassenvariablen Werte zugwiesen!!!
+   */
+
+
+  // Methoden
+  public void gasgeben(){
+    System.out.println("Das Auto mit der Marke: " + this.marke + " gibt gas und fährt jetzt los.");
+  }
+
+  public void bremsen(){
+    System.out.println("Das Auto mit der Marke: " + this.marke + " bremst!");
+  }
+
+  public void vergleichePreis(int preis){
+    if (this.preis <= preis) {
+      System.out.println("Der " + this.marke + " ist billiger.");
+    } else {
+      System.out.println("Der " + this.marke + " ist teurer.");
+    }
+  }
+
+  /**
+   * Das Schlüsselwort "this" besagt: Gehe in das Objekt, das von der Klasse Auto erstellt wurde, rein und hole von
+   * der Variable "marke" den Wert raus.
+   *
+   * WICHTIG: In der OOP sollen Methoden immer von Objekten aufgerufen werden.
+   */
 
 }
--- Udemy-Java-Kurs-OOP_I/src/com/company/Main.java
+++ Udemy-Java-Kurs-OOP_I/src/com/company/Main.java
@@ -12,50 +12,71 @@ public class Main { // übersetzt: Haupt-Klasse
      * Sinnvollerweise werden Objekte von Klassen in der main-Methode erstellt. Dann kann im weiteren Verlauf des
      * Programms mit diesen Objekten gearbeitet werden.
      * ERINNERE: DIE main-Mathode ist der Startpunkt eines jeden Programms.
+     *
+     * WICHTIG: (ANDERE) METHODEN
+     * In der Objektorientierten Programmierung (OOP) werden Methoden immer von deren Objekten aufgerufen werden.
+     * Daher wie folgt vorgehen:
+     * (1.) Ein Objekt einer Klasse erzeugen.
+     * (2.) Von diesem Objekt aus die Methoden aufrufen.
      */
 
-    // Im Vergleich Variablen vs. Klassen
-    int zahl = 50;
-    System.out.println(zahl);
-    // In der Konsole wird lediglich 50 ausgegeben, da in dieser primitiven VAR nur ein Wert gespeichert ist.
+    // BISHER: VARIABLEN deklarieren und initialisieren: Datentyp Name = Wert;
+     // In der Konsole wird lediglich 50 ausgegeben, da in dieser primitiven VAR nur ein Wert gespeichert ist.
+     int zahl = 50;
+     System.out.println(zahl);
 
-    // Wie wird ein Objekt erstellt?
-    // Datentyp variablenName = new Konstruktor();
+     // JETZT: OBJEKTE deklarieren und initialisieren: Datentyp variablenName = new Konstruktor();
+    // ERSTELLUNG EINES OBJEKTS anhand einer Klasse mit dem Konstruktor für diese Klasse.
+    // MERKE:
     Auto bmw = new Auto();
     bmw.preis = 5000;
     bmw.marke = "BMW";
-    System.out.println(bmw); // Siehe Erklärung ganz unten!
+    System.out.println(bmw); //  Gibt die Speicheradresse des Objekts "bmw" aus (Indentity Hash-Codde. Siehe unten!
     System.out.println("Preis: " + bmw.preis + ", Marke: " + bmw.marke);
 
-    /** Bedeutung der obigen Befehlszeile:
-     * Auto => Das ist die Klasse (Name der Klasse), von der ein Objekt erstellt werden soll.
-     * bmw => Name der Variable, in der das neue Objekt zugewiesen/gespeichert werden soll, damit wir darüber
-     * auf das Objekt zugreifen können.
+    /** Bedeutung der Befehlszeile: Auto bmw = new Auto()
+     * bmw => Name der Variable, in der das neue Objekt zugewiesen/gespeichert werden soll, damit wir darüber auf
+     * das Objekt zugreifen können. Diese VAR ist ein komplexer Datentyp und kann mehrere Daten/Werte speichern.
+     * Diese Variable wird auch Referenzvariable genannt, weil es auf den Speicherplatz mit dem Objekt verweist.
+     * Wenn die Referenzvariable println ausgegeben wird, dann wir die Adresse des Speicherplatzes angezeigt. Damit
+     * einzelne Inhalte angezeigt werden, muss in diese VAR mit dem .-Operator zugegriffen werden.
+     *
+     * Auto => Datentyp der Variable "bmw"!!! Wie ist der Datentyp "Auto" definiert? Es ist eine Klasse!!! Aus dieser
+     * Klasse werden wir ein Objekt erstellen. Die Variablen in der Klasse bestimmen die Eigenschaften/Attribute
+     * des Objekts.
+     *
      * '=' => Der übliche Zuweisungsoperator, damit der Wert rechts davon in die VAR links davon zugewiesen wird.
-     * new => Schlüsselwort, mit der ein neues Objekt erzeugt wird. Es reserviert Arbeitsspeicher Platz für das Objekt.
+     *
+     * new => Schlüsselwort bzw. new-Operator mit der ein neues Objekt erzeugt wird. Es reserviert einen Platz
+     * im Arbeitsspeicher des Computers Platz für das zu generierende Objekt.
+     *
      * Konstruktor() => Initialisiert ein Objekt mit Werten (im obigen Fall noch nicht, da () leer ist.
-     * Der Konstruktor muss genauso heißen wie die Klasse selbst. Z.B. Auto()
+     * Der Konstruktor muss genauso heißen wie die Klasse selbst, also: Auto()
+     * Mit dem Aufruf des Konstruktors erstellen wir ein Objekt. Ohne Parameterwerte wird ein Standard Objekt Auto
+     * erstellt. Wenn beim Aufrufen des Konstrukturs ihm auch Parameterwerte übergeben werden, so wird ein individuelles
+     * Objekt erstellt.
      */
 
     /** REFERENZVARIABLE
      *
      * Java ist eine Objekt-orientierte Programmiersprache (kurz: OOP), bei der es darum geht, Klassen zu definieren
-     * und daraus eine gewünschte Anzahl Objekte zu generieren.
+     * und daraus Objekte zu erstellen. OOP orientiert sich an der reellen Welt und den Objekten dort.
      * Eine Klasse kann man sich wie einen Bauplan vorstellen, nach der man immer wieder neue Objekte erzeugen kann.
-     * Welche Eigenschaften ein Objekt haben kann, sind in der Klasse definiert.
+     * Welche Eigenschaften ein Objekt haben kann, ist in der Klasse definiert.
+     * Objekte sind wie aus der realen Welt abgeleitet. Beispiele: Auto, Haus, Tier, Menschen usw.
      *
      * Eine Klasse kann auch als ein komplexer Datentyp verstanden werden, in der mehrere Daten/Werte unterschiedlicher
-     * Datentypen gespeichert werden können.
+     * Datentypen gespeichert werden können. Daher können wir in der Definition einer Klasse mehrere Variablen
+     * festlegen, welche als Eigenschaften (in der Programmierung Attribute genannt) das Objekt bestimmten.
      *
      * Das generierte Objekt einer Klasse wird einer Variable zugewiesen. In dieser Variable sind keine Werte
      * gespeichert, sondern nur eine Adresse, welcher auf das Objekt referenziert. Und an dieser Adresse sind
-     * die ganzen Variablen bzw. deren Werte gespeichert, die zum Objekt. Die Werte sind in Variablen gespeichert, die bei der
-     * Erstellung der Klasse definiert wurden.
+     * die ganzen Variablen bzw. deren Werte gespeichert, die zum Objekt gehören.
      * Daher wird die Variable eines Objekts "Referenzvariable" genannt und die darin gespeicherte Adresse
      * "Referenzadresse".
      *
      * Wenn man den Inhalt der Referenzvariable in der Konsole ausgibt, erhält man seinen Indentity-Hash-Code,
-     * der einzigartig für jedes Objekt einer Klasse ist.
+     * der einzigartig für jedes Objekt einer Klasse ist und damit die Speicheradresse ist.
      * MERKE: Der Identity-Hash-Code ist wie ein Strichcode auf Produkten im Supermarkt.
      *
      * Auf die einzelnen Werte eines Objekts wird über seine Referenzvariable zugriffen. Dabei wird mit dem .-Operator
@@ -71,18 +92,28 @@ public class Main { // übersetzt: Haupt-Klasse
      * abrufen können.
      */
 
+    // A. STANDARD-KONSTRUKTOR AUFRUFEN (ohne Parameterwerte):
+    Auto vw = new Auto(); // Ein Objekt der Klasse Auto wird generiert: vw.
+    vw.preis = 5000; // Der VAR "preis" im Objekt "vw" wird der Wert 5000 zugewiesen (initialisiert).
+    vw.marke = "VW"; // Der VAR "marke" im Objekt "marke" wird der Wert "VW" zugewiesen (initialisiert).
+    vw.vergleichePreis(2000);
 
-    Auto audi = new Auto();
-    audi.preis = 6000;
-    audi.marke = "Audi";
-
-    System.out.println("Preis: " + audi.preis + ", Marke: " + audi.marke);
-
-    Auto vw = new Auto();
-    vw.preis = 3000;
-    vw.marke = "VW";
-
+    System.out.println(bmw); // Gibt die Speicheradresse der Referenzvariable "bmw" aus: com.company.Auto@2d6a9952
+    System.out.println(bmw.preis); // Geht in die VAR/das Objekt "bmw" rein und holt den Wert der VAR "preis".
     System.out.println("Preis: " + vw.preis + ", Marke: " + vw.marke);
+
+    // B. KONSTRUKTOR MIT PARAMETERWERTEN AUFRUFEN (Übergabe von Parameterwerten):
+    // Ein Objekt der Klasse Auto wird erstellt: "audi" und erhält direkt Parameterwerte (Eigenschaften).
+    Auto audi = new Auto(4, 5000, "Audi");
+    System.out.println("Reifenanzahl: " + audi.reifen + ", Preis: " + audi.preis + ", Marke: " + audi.marke);
+
+    // MERKE: In der Objektorientierten Programmierung (OOP) werden Methoden immer von Objekten aufgerufen.
+    // Das Objekt audi von der Klasse Auto haben wir oben erzeugt. Jetzt nutzen wir die Methoden, die in der Klasse
+    // Auto definiert sind.
+
+    audi.gasgeben();
+    audi.bremsen();
+    audi.vergleichePreis(4000);
 
   }
 }
